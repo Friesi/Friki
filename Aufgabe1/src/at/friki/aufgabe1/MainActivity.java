b6a570f0068183a09b7154b064eabdebe9d405d7
Friesi <dl11@gmx.net>
2013-09-27 09:56:43 +0200

    Init

--- Aufgabe1/src/at/friki/aufgabe1/MainActivity.java
+++ Aufgabe1/src/at/friki/aufgabe1/MainActivity.java
@@ -1,22 +1,43 @@
-package at.friki.aufgabe1;
+package at.friki.exercise1;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    public void btnStart(View v) {
+
+        SaxRssFeedParser rss = new SaxRssFeedParser("http://derStandard.at/?page=rss&ressort=Webstandard");
+        List<RssItem> items = new ArrayList<RssItem>();
+
+        items = rss.parse();
+
+        TextView tv = (TextView) this.findViewById(R.id.textView);
+        tv.setText("");
+
+        for(RssItem item: items) {
+            tv.append(item.getTitle());
+        }
+    }
 }
