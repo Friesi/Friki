b2cc8ae18b34e9b725035acc8809165c32fbce24
Friesi <dl11@gmx.net>
2013-10-02 14:40:13 +0200

    Button Save eingebaut

--- Aufgabe1/src/at/friki/aufgabe1/FragmentSubscribe.java
+++ Aufgabe1/src/at/friki/aufgabe1/FragmentSubscribe.java
@@ -4,42 +4,42 @@ package at.friki.aufgabe1;
  * Created by Chris on 26.09.13.
  */
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.app.ListFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
-public class FragmentSubscribe extends Fragment {
+public class FragmentSubscribe extends Fragment implements OnClickListener {
 
 	public static final String BROADCAST_FRAGMENT_SUBSCRIBE_CLICK = "BROADCAST_FRAGMENT_SUBSCRIBE_CLICK";
     
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.subscribe_layout, container, false);
+      
+      Button btnSave = (Button) view.findViewById(R.id.btnSubscribeSave);
+      btnSave.setOnClickListener(this);
+      
       return view;
     }
+    
+    @Override
+    public void onClick(View v) {
+    	//Intent subscribeintent = new Intent(BROADCAST_FRAGMENT_SUBSCRIBE_CLICK);
+    	//LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(subscribeintent);
+    	AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    	builder.setMessage("RSS Abonnieren?").setPositiveButton("Ja", dialogClickListener).setNegativeButton("Nein", dialogClickListener).show();		//Dialog zeigen
+    }  
           
 
-	//AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-	//builder.setMessage("RSS Abonnieren?").setPositiveButton("Ja", dialogClickListener).setNegativeButton("Nein", dialogClickListener).show();		//Dialog zeigen
-    	
+		
     DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
@@ -64,6 +64,7 @@ public class FragmentSubscribe extends Fragment {
             }
         }
     };
+    
     
     
     
